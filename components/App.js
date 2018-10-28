71b6d5d25a07068be4f09b3279712c10c9f5b031
Leszek Macura <leszek@macura.co>
2018-10-28 16:53:41 +0100

    Zadanie 14_06 Poprawione

--- components/App.js
+++ components/App.js
@@ -18,6 +18,41 @@ var Counter = React.createClass({
         });
     },
 
+
+    //Stany do wyświetlenia w console log 
+
+    getDefaultProps: function () {
+        console.log('getDefaultProps - ustawienie domyślnych propsów');
+    },
+
+    componentWillMount: function () {
+        console.log('componentWillMount - jest wywoływana tuż przed wystąpieniem montowania w stanie przed renderowaniem');
+    },
+
+    componentDidMount: function () {
+        console.log('componentDidMount - jest wywoływany natychmiast po zamontowaniu komponentu (wstawiony do drzewa) - metoda działa po wygenerowaniu DOMu');
+    },
+
+    componentWillReceiveProps: function () {
+        console.log('componentWillReceiveProps - jest wywoływany, zanim zamontowany komponent otrzyma nowe rekwizyty');
+    },
+
+    shouldComponentUpdate: function () {
+        console.log('shouldComponentUpdate - jest wywoływana przed renderowaniem uaby React wiedział, czy aktualna zmiana stanu lub rekwizytów nie wpływa na wynik komponentu');
+        return true;
+    },
+
+    componentWillUpdate: function () {
+        console.log('componentWillUpdate - jest wywoływana tuż przed renderowaniem, gdy odbierane są nowe rekwizyty lub stan');
+    },
+
+    componentWillUnmount: function () {
+        console.log('componentWillUnmount - jest wywoływany bezpośrednio przed odmontowaniem i zniszczeniem komponentu - metoda działa po wyrenderowaniu komponentu do DOM');
+    },
+
+
+    // dalsza czesc proramu liczników
+
     render: function () {
         return (
             React.createElement('div', { className: 'counter' },
